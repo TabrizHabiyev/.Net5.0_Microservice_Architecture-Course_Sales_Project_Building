779410dd77992b0409b1c7b02c183d63b6e2eeb4
TabrizHabiyev <tabrizfh@code.edu.az>
2022-05-24 03:48:36 +0400

    Settings for database connection are configured

--- Services/Catalog/FreeCourse.Services.Catalog/Dtos/FeatureDto.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/FeatureDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace FreeCourse.Services.Catalog.Dtos
 {
-    public class FeaturedDto
+    public class FeatureDto
     {
         public int Duration { get; set; }
     }
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Services.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string CourceCollectionName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
