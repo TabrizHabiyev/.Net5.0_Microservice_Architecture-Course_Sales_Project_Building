6154d5178e088028025614268931eda6143ac7f5
TabrizHabiyev <tabrizfh@code.edu.az>
2022-05-25 20:32:48 +0400

    feat: create Category Controller

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryServices _categoryServices;
+
+        public CategoriesController(ICategoryServices categoryServices)
+        {
+            _categoryServices = categoryServices;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryServices.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _categoryServices.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var response = await _categoryServices.CreateAsync(categoryDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using FreeCourse.Services.Catalog.Models;
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
 using FreeCourse.Services.Catalog.Services;
 using FreeCourse.Shared;
 using Microsoft.AspNetCore.Http;
@@ -33,13 +34,40 @@ namespace FreeCourse.Services.Catalog.Controllers
             return CreateActionResultInstance(response);
         }
 
-        [HttpGet("{id}")]
+        [Route("/api/[controller]GetAllByUserId/{userId}")]
         public async Task<IActionResult> GetAllByUserId(string userId)
         {
-            var response = await _courseServices.GetAllByUserId(userId);
+            var response = await _courseServices.GetAllByUserIdAsync(userId);
 
             return CreateActionResultInstance(response);
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreateDto)
+        {
+            var response = await _courseServices.CreateAsync(courseCreateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CourseUpdateDto courseCreateDto)
+        {
+            var response = await _courseServices.UpdateAsync(courseCreateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseServices.DeleteAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
 
     }
 }
